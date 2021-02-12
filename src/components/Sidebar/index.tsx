b182648a1abcfe0be76fd05daf2b31035b86779c
Igor Sales <igorsales.fs@gmail.com>
2021-02-12 02:11:43 -0300

    add NavLink Component and configure activeClassName in css

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import { FaCog, FaColumns, FaRegCalendarAlt, FaRegEnvelope, FaRegSmile, FaSignOutAlt } from 'react-icons/fa';
 
