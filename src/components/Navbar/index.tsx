68aa4c7611d43efadf5ce25dc97baa98e3f5a686
Igor Sales <igorsales.fs@gmail.com>
2021-02-12 02:11:30 -0300

    add NavLink Component and configure activeClassName in css

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ApplicationState } from '../../store';
 
